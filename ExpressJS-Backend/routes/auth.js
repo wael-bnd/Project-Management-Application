debfd7c522a80015a09014a8c94bb7719f3f6904
Wael-Ibn-Douma <119068213+Wael-Ibn-Douma@users.noreply.github.com>
2023-05-24 10:54:17 +0100

    user login and logout dev completed

--- ExpressJS-Backend/routes/auth.js
+++ ExpressJS-Backend/routes/auth.js
@@ -5,5 +5,7 @@ const authController = require("../controllers/auth");
 const { userSignupValidator } = require("../validators");
 
 router.post("/signup", userSignupValidator, authController.signup);
+router.post("/signin", authController.signin);
+router.get("/signout", authController.requireSignin, authController.signout);
 
 module.exports = router;
