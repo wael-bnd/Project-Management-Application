6c9cfbbc67afe20c38333161dd999b52428ee69a
Wael-Ibn-Douma <119068213+Wael-Ibn-Douma@users.noreply.github.com>
2023-05-24 14:58:15 +0100

    creating Task model

---
+++ ExpressJS-Backend/models/Task.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Schema;
+
+const taskSchema = new mongoose.Schema(
+  {
+    summary: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    issueType: {
+      type: String,
+      enum: ["story", "test", "bug"],
+      required: true,
+    },
+    estimation: {
+      type: Number,
+      enum: [1, 2, 3, 5, 8, 13, 20],
+    },
+    reporter: {
+      type: ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignee: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
