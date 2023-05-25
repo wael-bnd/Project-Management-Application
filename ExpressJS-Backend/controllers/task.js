499fa06029204179b5638247c6d43fdd10c0fff8
Wael-Ibn-Douma <119068213+Wael-Ibn-Douma@users.noreply.github.com>
2023-05-25 14:07:57 +0100

    adding setAssignee and setReporter methods

--- ExpressJS-Backend/controllers/task.js
+++ ExpressJS-Backend/controllers/task.js
@@ -1,4 +1,5 @@
 const Task = require("../models/Task");
+const User = require("../models/User");
 
 exports.createTask = async (req, res) => {
   try {
@@ -73,7 +74,70 @@ exports.updateTask = async (req, res) => {
     });
   }
 };
+exports.setTaskAssignee = async (req, res) => {
+  try {
+    const task = req.task;
+    const assignee = req.body.assignee.toString();
+    const isReporter = task.reporter.toString() === req.auth._id.toString();
+    const selfAssign = assignee === req.auth._id.toString();
 
+    if (!isReporter && !selfAssign) {
+      return res.status(403).json({
+        error:
+          "You are not authorized to perform this action. You can only assign to yourself.",
+      });
+    }
+
+    const user = await User.findById(assignee);
+    if (!user || !assignee) {
+      return res.status(404).json({
+        error: "User not found!",
+      });
+    }
+    task.assignee = assignee;
+    await task.save();
+    res.status(200).json({
+      message: "Task assigned successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "An error occurred while updating the task.",
+    });
+  }
+};
+
+exports.setTaskReporter = async (req, res) => {
+  try {
+    const task = req.task;
+    const reporter = req.body.reporter.toString();
+    const isReporter = task.reporter.toString() === req.auth._id.toString();
+    const selfAssign = reporter === req.auth._id.toString();
+
+    if (!isReporter && !selfAssign) {
+      return res.status(403).json({
+        error: "You are not authorized to perform this action. ",
+      });
+    }
+
+    const user = await User.findById(reporter);
+    if (!user || !reporter) {
+      return res.status(404).json({
+        error: "User not found!",
+      });
+    }
+
+    task.reporter = reporter;
+    await task.save();
+
+    res.status(200).json({
+      message: "Task repoter updated successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "An error occurred while updating the task.",
+    });
+  }
+};
 exports.taskById = async (req, res, next, id) => {
   try {
     const task = await Task.findById(id);
